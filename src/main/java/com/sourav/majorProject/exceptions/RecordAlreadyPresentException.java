cf5f16b33c4c27ffca02950f33060a8591edf532
Sourav <souravgopal25@gmail.com>
2021-10-09 19:35:36 +0530

    Multiple Exceptions Added

---
+++ src/main/java/com/sourav/majorProject/exceptions/RecordAlreadyPresentException.java
@@ -0,0 +1,7 @@
+package com.sourav.majorProject.exceptions;
+
+public class RecordAlreadyPresentException extends RuntimeException {
+	public RecordAlreadyPresentException(String s) {
+		super(s);
+	}
+}
